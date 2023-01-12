8b587328d6046f3f13aa8f48d1022762b3ee8321
elis-11 <eliza.arzanukaeva@gmail.com>
2023-01-12 14:04:39 +0100

    Filter Cars

--- src/App.tsx
+++ src/App.tsx
@@ -2,11 +2,37 @@ import { useEffect, useState } from "react";
 import "./App.scss";
 import carsJson from "./assets/cars.json";
 
+type Car = {
+id: string,
+name: string,
+url: string,
+year: number,
+}
+
 function App() {
-  const [cars, setCars] = useState(carsJson);
-  const [selectedYear, setSelectedYear] = useState();
+  const [cars, setCars] = useState<[Car]>(carsJson as [Car]);
+  const [selectedYear, setSelectedYear] = useState<number | undefined>();
   console.log(cars);
 
+
+  const filterByYear = (filteredData: [Car]) => {
+    if (!selectedYear) {
+      return filteredData;
+    }
+    const filteredCars = filteredData.filter(
+      (car) => car.year === selectedYear
+    );
+    return filteredCars;
+  };
+  const handleYearChange = (year: number) => {
+    console.log(year);
+    if (year === selectedYear) {
+      setSelectedYear(undefined);
+    } else {
+      setSelectedYear(year);
+    }
+  };
+let filteredCars= filterByYear(cars)
   return (
     <div className="Cars">
       <h1>Selected Cars</h1>
@@ -16,6 +42,7 @@ function App() {
           {[2018, 2019, 2020].map((year) => (
             <div
               key={year}
+              onClick={() => handleYearChange(year)}
               className={selectedYear === year ? "active" : "filter"}
             >
               {year}
@@ -25,7 +52,7 @@ function App() {
       </div>
 
       <div className="cars">
-        {cars.map((car) => (
+        {filteredCars.map((car) => (
           <div key={car.id} className="car">
             <div className="name">{car.name}</div>
             <div className="year">{car.year}</div>
